90743790fe1a431fb51fc5dfd3ddd34120b59191
Alex Rea <amplitude@gmail.com>
2011-05-11 08:31:21 +0100

    Fullscreen, starting to work on GUI
    Using PGU for the GUI as it seems to be the most complete library.
    No active components in the GUI yet, just a label to check that it
    was being rendered correctly.
    
    Escape now quits the full-screen application.

--- dalek.py
+++ dalek.py
@@ -7,6 +7,9 @@ import opencv
 #this is important for capturing/displaying images
 from opencv import highgui
 
+#GUI
+from pgu import gui
+
 #Velleman board
 from pyk8055 import *
 
@@ -18,13 +21,31 @@ def get_image():
     #convert Ipl image to PIL image
     return opencv.adaptors.Ipl2PIL(im)
 
+class DalekControl(gui.Table):
+	def __init__(self,**params):
+		gui.Table.__init__(self,**params)
+		fg=(255,255,255)
+ 		self.tr()
+        	self.td(gui.Label("Dalek GUI",color=fg),colspan=2)
+
+
+
+
 fps = 30.0
 pygame.init()
-window = pygame.display.set_mode((640,480))
-pygame.display.set_caption("WebCam Demo")
+window = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+pygame.display.set_caption("DalekCam")
 screen = pygame.display.get_surface()
 
 pygame.joystick.init()
+
+form = gui.Form()
+app = gui.App()
+dalekCtrl = DalekControl()
+c=gui.Container(align=-1,valign=-1)
+c.add(dalekCtrl,700,30)
+
+app.init(c)
 
 try:
 	j=pygame.joystick.Joystick(0)
@@ -38,11 +59,15 @@ try:
 except IOError:
 	print 'could not find K8055 board'
 
-
-while 1:
+done=False
+while not done:
 	for e in pygame.event.get():
 		print e
-		if e.type == pygame.locals.JOYAXISMOTION:
+		if e.type is pygame.locals.QUIT:
+			done=True
+		elif e.type is pygame.locals.KEYDOWN and e.key == pygame.locals.K_ESCAPE:
+			done=True	
+		elif e.type == pygame.locals.JOYAXISMOTION:
 			x,y = j.get_axis(0), j.get_axis(1)
 			print 'x and y : ' + str(x) + ' , ' + str(y)
 		elif e.type == pygame.locals.JOYBALLMOTION:
@@ -56,10 +81,13 @@ while 1:
 				k.WriteAllDigital(2)	
 		elif e.type == pygame.locals.JOYBUTTONUP:
 			print 'buttonup'
+		else:
+			app.event(e) #pass it to the GUI
 
 	im = get_image()
 	pg_img = pygame.image.frombuffer(im.tostring(), im.size, im.mode)
 	screen.blit(pg_img, (0,0))
+	app.paint()
 	pygame.display.flip()
 	pygame.time.delay(int(1000 * 1.0/fps))
 
